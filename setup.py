4c67abaee0d722940053e07700f7f56bd5108e7c
Chris Dent <chris.dent@gmail.com>
2010-10-22 16:34:49 +0100

    Prep 0.9.16 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ AUTHOR = 'Chris Dent'
 AUTHOR_EMAIL = 'cdent@peermore.com'
 NAME = 'tiddlywebplugins.sqlalchemy'
 DESCRIPTION = 'sqlalchemy store for tiddlyweb'
-VERSION = '0.9.15' # make sure you update in __init__ too
+VERSION = '0.9.16' # make sure you update in __init__ too
 
 
 import os
