f9f755ddea0928ad0f86007185e6a5ca5e17709f
Chris Dent <chris.dent@gmail.com>
2011-11-12 19:00:41 +0000

    release 2.0.9

--- tiddlywebplugins/sqlalchemy2/__init__.py
+++ tiddlywebplugins/sqlalchemy2/__init__.py
@@ -26,7 +26,7 @@ from tiddlyweb.store import (NoBagError, NoRecipeError, NoTiddlerError,
 from tiddlyweb.stores import StorageInterface
 from tiddlyweb.util import binary_tiddler
 
-__version__ = '2.0.8'
+__version__ = '2.0.9'
 
 #logging.basicConfig()
 #logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
