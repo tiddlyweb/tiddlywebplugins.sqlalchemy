11eb4d978aafd3b3834a3bbfb10dbc5305753803
Chris Dent <chris.dent@gmail.com>
2012-04-09 18:54:19 +0100

    Add a DELETE cascade for the bag and recipe _policy tables.
    This may fix 922 in the tiddlyspace repo, by making sure that
    rows are deleted in a more atomic fashion.

--- tiddlywebplugins/sqlalchemy3/__init__.py
+++ tiddlywebplugins/sqlalchemy3/__init__.py
@@ -39,14 +39,14 @@ Session = scoped_session(sessionmaker())
 bag_policy_table = Table('bag_policy', Base.metadata,
     Column('bag_id', Integer, ForeignKey('bag.id', ondelete='CASCADE'),
         index=True, nullable=False, primary_key=True),
-    Column('policy_id', Integer, ForeignKey('policy.id'), nullable=False,
-        primary_key=True))
+    Column('policy_id', Integer, ForeignKey('policy.id', ondelete='CASCADE'),
+        nullable=False, primary_key=True))
 
 recipe_policy_table = Table('recipe_policy', Base.metadata,
     Column('recipe_id', Integer, ForeignKey('recipe.id', ondelete='CASCADE'),
         index=True, nullable=False, primary_key=True),
-    Column('policy_id', Integer, ForeignKey('policy.id'), nullable=False,
-        primary_key=True))
+    Column('policy_id', Integer, ForeignKey('policy.id', ondelete='CASCADE'),
+        nullable=False, primary_key=True))
 
 current_revision_table = Table('current_revision', Base.metadata,
         Column('tiddler_id', Integer, ForeignKey('tiddler.id',
