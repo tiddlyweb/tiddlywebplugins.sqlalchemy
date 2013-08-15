80919552b7e45ea5402a12fedb21a200973e98d1
Chris Dent <chris.dent@gmail.com>
2013-08-15 16:03:48 +0100

    release 3.1.1
    * Take the limit off UnicodeText in text table

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ AUTHOR = 'Chris Dent'
 AUTHOR_EMAIL = 'cdent@peermore.com'
 NAME = 'tiddlywebplugins.sqlalchemy3'
 DESCRIPTION = 'sqlalchemy store for tiddlyweb'
-VERSION = '3.1.0' # make sure you update in __init__ too
+VERSION = '3.1.1' # make sure you update in __init__ too
 
 
 import os
